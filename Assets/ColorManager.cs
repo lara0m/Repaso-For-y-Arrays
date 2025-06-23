3f16a5254fb0dd9011cf908daa403cf5a28ee55c
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2025-06-23 12:18:15 -0300

    color y numero random

---
+++ Assets/ColorManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    public Color[] colors;
+    public Renderer cubeRenderer;
+    public int randomColorIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(SetRandomColor), 0,.5f);
+    }
+
+    void SetRandomColor()
+    {
+        randomColorIndex = Random.Range(0, colors.Length);
+        cubeRenderer.material.color = colors[randomColorIndex];
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] carteles;
+    public int RandomElement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(ActivateRandomElement),0, 2f);
+    }
+
+    void DeactivateAll()
+    {
+
+        RandomElement = Random.Range(0, carteles.Length);
+        for (int i = 0; i<carteles.Length; i++)
+        {
+            carteles[i].SetActive(false);
+        }
+    }
+
+    void ActivateRandomElement()
+    {
+        DeactivateAll();
+        carteles[RandomElement].SetActive(true);
+    }
+}
